d0d8e40abf91ae89353a18fc4658c6ffc828e85c
James Bradlee <james@nslo.no>
2025-12-22 02:40:39 +0100

    Refactor Gradle build script to register tasks after evaluation for sources and javadoc JARs

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,64 +26,66 @@ subprojects {
         apply(plugin = "maven-publish")
         apply(plugin = "signing")
 
-        val sourcesJar by tasks.register<Jar>("sourcesJar") {
-            dependsOn(tasks.getByName("classes"))
-            archiveClassifier.set("sources")
-            from(sourceSets["main"].allSource)
-        }
-
-        val dokkaHtmlJar by tasks.registering(Jar::class) {
-            from(tasks.dokkaGeneratePublicationHtml.flatMap { it.outputDirectory })
-            archiveClassifier.set("javadoc")
-        }
-
-        signing {
-            sign(publishing.publications)
-            useInMemoryPgpKeys(
-                System.getenv("SIGNING_KEY_ID"),
-                System.getenv("SIGNING_KEY"),
-                System.getenv("SIGNING_PASSWORD"),
-            )
-        }
-
-        publishing {
-            repositories {
-                maven {
-                    name = "central"
-                    url = uri("https://central.sonatype.com/api/v1/publisher")
-                    credentials {
-                        username = System.getenv("CENTRAL_TOKEN_USERNAME")
-                        password = System.getenv("CENTRAL_TOKEN_PASSWORD")
-                    }
-                }
+        afterEvaluate {
+            val sourcesJar by tasks.register<Jar>("sourcesJar") {
+                dependsOn(tasks.getByName("classes"))
+                archiveClassifier.set("sources")
+                from(sourceSets["main"].allSource)
             }
 
-            publications.create<MavenPublication>("central") {
-                from(components["kotlin"])
-                artifact(sourcesJar)
-                artifact(dokkaHtmlJar)
+            val dokkaHtmlJar by tasks.registering(Jar::class) {
+                from(tasks.dokkaGeneratePublicationHtml.flatMap { it.outputDirectory })
+                archiveClassifier.set("javadoc")
+            }
 
-                pom {
-                    name.set(project.name)
-                    description.set(requireNotNull(project.description))
-                    url.set("https://logfmt.tools")
+            signing {
+                sign(publishing.publications)
+                useInMemoryPgpKeys(
+                    System.getenv("SIGNING_KEY_ID"),
+                    System.getenv("SIGNING_KEY"),
+                    System.getenv("SIGNING_PASSWORD"),
+                )
+            }
 
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("https://opensource.org/licenses/MIT")
+            publishing {
+                repositories {
+                    maven {
+                        name = "central"
+                        url = uri("https://central.sonatype.com/api/v1/publisher")
+                        credentials {
+                            username = System.getenv("CENTRAL_TOKEN_USERNAME")
+                            password = System.getenv("CENTRAL_TOKEN_PASSWORD")
                         }
                     }
+                }
 
-                    scm {
-                        url.set("https://github.com/jamesbradlee/tools.logfmt")
-                        connection.set("scm:git:https://github.com/jamesbradlee/tools.logfmt.git")
-                    }
+                publications.create<MavenPublication>("central") {
+                    from(components["kotlin"])
+                    artifact(sourcesJar)
+                    artifact(dokkaHtmlJar)
 
-                    developers {
-                        developer {
-                            id.set("jamesbradlee")
-                            name.set("James Bradlee")
+                    pom {
+                        name.set(project.name)
+                        description.set(requireNotNull(project.description))
+                        url.set("https://logfmt.tools")
+
+                        licenses {
+                            license {
+                                name.set("MIT License")
+                                url.set("https://opensource.org/licenses/MIT")
+                            }
+                        }
+
+                        scm {
+                            url.set("https://github.com/jamesbradlee/tools.logfmt")
+                            connection.set("scm:git:https://github.com/jamesbradlee/tools.logfmt.git")
+                        }
+
+                        developers {
+                            developer {
+                                id.set("jamesbradlee")
+                                name.set("James Bradlee")
+                            }
                         }
                     }
                 }
